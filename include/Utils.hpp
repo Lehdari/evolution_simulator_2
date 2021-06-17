2bf6912b72ad44f467f7e3adc5dab831c796f4af
Lehdari <miika.lehtimaki@gmail.com>
2021-06-18 00:52:57 +0300

    Add Utils.hpp

---
+++ include/Utils.hpp
@@ -0,0 +1,18 @@
+//
+// Project: evolution_simulator_2
+// File: Utils.hpp
+//
+// Copyright (c) 2020 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#ifndef EVOLUTION_SIMULATOR_UTILS_HPP
+#define EVOLUTION_SIMULATOR_UTILS_HPP
+
+
+#define EVOLUTION_SIMULATOR_RES(PATH) (std::string(EVOLUTION_SIMULATOR_RES_DIR) + "/" + PATH)
+
+
+#endif //EVOLUTION_SIMULATOR_UTILS_HPP
