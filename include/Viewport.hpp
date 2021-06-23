6d73a75b48c3a9f4e528b61364429c9e5497c3c9
Lehdari <miika.lehtimaki@gmail.com>
2021-06-23 23:17:07 +0300

    Add Viewport class

---
+++ include/Viewport.hpp
@@ -0,0 +1,47 @@
+//
+// Project: evolution_simulator_2
+// File: Viewport.hpp
+//
+// Copyright (c) 2021 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#ifndef EVOLUTION_SIMULATOR_2_VIEWPORT_HPP
+#define EVOLUTION_SIMULATOR_2_VIEWPORT_HPP
+
+
+#include <gut_utils/MathTypes.hpp>
+
+
+class Viewport {
+public:
+    Viewport(const Vec2f& position, float scale);
+
+    /*  @brief  Multiply current scale
+     *  @param  scale   Scaling factor by which to modify the current scale
+     *  @param  minScale    Minimum allowed scale level
+     *  @param  maxScale    Maximum allowed scale level
+     */
+    void applyScale(float scale, float minScale=0.5f, float maxScale=300.0f);
+
+    /*  @brief  Zoom the viewport with respect to a pivot point
+     *  @param  zoom    Scaling factor, similar to scale in applyScale
+     *  @param  pivot   Pivot point used as a centroid for zooming, in pixel coordinates
+     *  @param  minScale    Minimum allowed scale level
+     *  @param  maxScale    Maximum allowed scale level
+     */
+    void zoom(float scale, const Vec2f& pivot, float minScale=0.5f, float maxScale=300.0f);
+
+    const Mat3f& getViewport() const;
+
+    operator const Mat3f&() const;
+
+private:
+    float   _initialScale;
+    Mat3f   _viewport;
+};
+
+
+#endif //EVOLUTION_SIMULATOR_2_VIEWPORT_HPP
---
+++ src/Viewport.cpp
@@ -0,0 +1,69 @@
+//
+// Project: evolution_simulator_2
+// File: Viewport.cpp
+//
+// Copyright (c) 2021 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENSE which is distributed
+// with this source code package.
+//
+
+#include <Viewport.hpp>
+
+
+Viewport::Viewport(const Vec2f& position, float scale) :
+    _initialScale   (scale)
+{
+    _viewport <<
+        scale,  0.0f,   position(0),
+        0.0f,   scale,  position(1),
+        0.0f,   0.0f,   1.0f;
+}
+
+void Viewport::applyScale(float scale, float minScale, float maxScale)
+{
+    float s = _viewport(0,0)*scale;
+    if ((s-_initialScale)*(s-_initialScale) < 1.0e-6)
+        s = _initialScale;
+
+    if (s < minScale || s > maxScale)
+        return;
+
+    _viewport(0,0) = s;
+    _viewport(1,1) = s;
+}
+
+void Viewport::zoom(float scale, const Vec2f& pivot, float minScale, float maxScale)
+{
+    float s = _viewport(0,0)*scale; // new scaling factor
+
+    // return to initial scale for numerical stability
+    if ((s-_initialScale)*(s-_initialScale) < 1.0e-6)
+        s = _initialScale;
+
+    // scaling limits
+    if (s < minScale || s > maxScale)
+        return;
+
+    // zoom transform
+    Mat3f z;
+    z <<    scale,  0.0f,   pivot(0)-scale*pivot(0),
+            0.0f,   scale,  pivot(1)-scale*pivot(1),
+            0.0f,   0.0f,   1.0f;
+
+    _viewport = z * _viewport;
+
+    // set scaling to the stabilized factor
+    _viewport(0,0) = s;
+    _viewport(1,1) = s;
+}
+
+const Mat3f& Viewport::getViewport() const
+{
+    return _viewport;
+}
+
+Viewport::operator const Mat3f&() const
+{
+    return _viewport;
+}
