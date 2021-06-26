d83c63ed72ad633be2ecfbb100e32b3a91dbd3c7
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2021-06-26 18:50:36 +0300

    Add FoodComponent struct

---
+++ include/FoodComponent.hpp
@@ -0,0 +1,20 @@
+//
+// Project: evolution_simulator_2
+// File: FoodComponent.hpp
+//
+// Copyright (c) 2021 Miika 'Lehdari' Lehtimäki
+// You may use, distribute and modify this code under the terms
+// of the licence specified in file LICENCE which is distributed
+// with this source code package.
+//
+
+#ifndef EVOLUTION_SIMULATOR_2_FOODCOMPONENT_HPP
+#define EVOLUTION_SIMULATOR_2_FOODCOMPONENT_HPP
+
+
+struct FoodComponent {
+
+};
+
+
+#endif //EVOLUTION_SIMULATOR_2_FOODCOMPONENT_HPP
