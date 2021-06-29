2b15e7c1c53c9a00cf9b8df641a5bd50e1af21de
Lehdari <miika.lehtimaki@gmail.com>
2021-06-30 02:18:49 +0300

    WorldSingleton: Implement entity fetching with an AABB

--- include/WorldSingleton.hpp
+++ include/WorldSingleton.hpp
@@ -25,22 +25,31 @@ public:
     void resetBVH();
     void addEntity(const fug::EntityId& eId, const Vec2f& position);
 
+    // get entities inside an AABB
+    void getEntities(Vector<fug::EntityId>& entities, const Vec2f& begin, const Vec2f& end) const;
+
     friend class BVHSystem;
 
 private:
     struct Node {
         fug::EntityId   eId = -1;
         uint64_t        mortonTail;
+        Vec2f           position;
         int64_t         children[4] = { -1, -1, -1, -1 };
     };
 
     float               _worldSize;
     Vector<Node>        _nodes;
 
-    void addNode(Vector<Node>& nodes, uint64_t nId,
-        const fug::EntityId& eId, uint64_t morton);
+    void addNode(uint64_t nId, const fug::EntityId& eId, uint64_t morton, const Vec2f& position);
 
-    inline __attribute__((always_inline)) uint64_t createMortonCode(const Vec2f& p);
+    void getEntitiesAll(uint64_t nId, Vector<fug::EntityId>& entities) const;
+    void getEntitiesBox(uint64_t nId, Vector<fug::EntityId>& entities,
+        const Vec2f& begin, uint64_t beginMorton,
+        const Vec2f& end, uint64_t endMorton,
+        uint64_t mode = 0) const;
+
+    inline __attribute__((always_inline)) uint64_t createMortonCode(const Vec2f& p) const;
     static inline __attribute__((always_inline)) uint64_t binaryExpand(uint32_t n);
 };
 
--- src/WorldSingleton.cpp
+++ src/WorldSingleton.cpp
@@ -11,18 +11,6 @@
 #include <WorldSingleton.hpp>
 
 
-//TODO TEMP
-template <typename T>
-void printBinary(T b) {
-    constexpr T l = sizeof(T)*8;
-    for (T i=0; i<l; ++i) {
-        T j = l-1-i;
-        printf("%lu", (b & ((T)1 << j)) >> j);
-    }
-}
-//TODO END OF TEMP
-
-
 WorldSingleton::WorldSingleton(float worldSize) :
     _worldSize  (worldSize),
     _nodes      (1)
@@ -38,17 +26,23 @@ void WorldSingleton::resetBVH()
 void WorldSingleton::addEntity(const fug::EntityId& eId, const Vec2f& position)
 {
     uint64_t morton = createMortonCode(position);
-    addNode(_nodes, 0, eId, morton);
+    addNode(0, eId, morton, position);
 }
 
-void WorldSingleton::addNode(
-    Vector<Node>& nodes, uint64_t nId, const fug::EntityId& eId, uint64_t morton)
+void WorldSingleton::getEntities(Vector<fug::EntityId>& entities, const Vec2f& begin, const Vec2f& end) const
 {
-    auto* node = &nodes[nId];
+    getEntitiesBox(0, entities, begin, createMortonCode(begin), end, createMortonCode(end), 15lu);
+}
+
+void WorldSingleton::addNode(uint64_t nId, const fug::EntityId& eId,
+    uint64_t morton, const Vec2f& position)
+{
+    auto* node = &_nodes[nId];
 
     if (node->eId == -1) {
         node->eId = eId;
         node->mortonTail = morton;
+        node->position = position;
         return;
     }
 
@@ -56,11 +50,12 @@ void WorldSingleton::addNode(
         // move the current entity
         uint64_t mId = node->mortonTail & 3llu;
         if (node->children[mId] < 0) {
-            node->children[mId] = nodes.size();
-            nodes.emplace_back();
-            node = &nodes[nId];
-            nodes.back().eId = node->eId;
-            nodes.back().mortonTail = node->mortonTail >> 2;
+            node->children[mId] = _nodes.size();
+            _nodes.emplace_back();
+            node = &_nodes[nId];
+            _nodes.back().eId = node->eId;
+            _nodes.back().mortonTail = node->mortonTail >> 2;
+            _nodes.back().position = node->position;
             node->eId = -2;
         }
     }
@@ -68,18 +63,81 @@ void WorldSingleton::addNode(
     // add the new entity
     uint64_t cId = morton & 3llu;
     if (node->children[cId] < 0) {
-        node->children[cId] = nodes.size();
-        nodes.emplace_back();
-        node = &nodes[nId];
-        nodes.back().eId = eId;
-        nodes.back().mortonTail = morton >> 2;
+        node->children[cId] = _nodes.size();
+        _nodes.emplace_back();
+        node = &_nodes[nId];
+        _nodes.back().eId = eId;
+        _nodes.back().mortonTail = morton >> 2;
+        _nodes.back().position = position;
         return;
     }
 
-    addNode(nodes, node->children[cId], eId, morton >> 2);
+    addNode(node->children[cId], eId, morton >> 2, position);
 }
 
-uint64_t WorldSingleton::createMortonCode(const Vec2f& p)
+void WorldSingleton::getEntitiesAll(uint64_t nId, Vector<fug::EntityId>& entities) const
+{
+    auto& node = _nodes[nId];
+
+    if (node.eId >= 0) {
+        entities.push_back(node.eId);
+        return;
+    }
+
+    for (auto& c : node.children) {
+        if (c >= 0)
+            getEntitiesAll(c, entities);
+    }
+}
+
+void WorldSingleton::getEntitiesBox(uint64_t nId, Vector<fug::EntityId>& entities,
+    const Vec2f& begin, uint64_t beginMorton,
+    const Vec2f& end, uint64_t endMorton,
+    uint64_t mode) const
+{
+    auto& node = _nodes[nId];
+
+    if (node.eId >= 0 &&
+        node.position(0) > begin(0) &&
+        node.position(1) > begin(1) &&
+        node.position(0) < end(0) &&
+        node.position(1) < end(1)) {
+        entities.push_back(node.eId);
+        return;
+    }
+
+    uint64_t morton = (beginMorton & 3llu) | ((endMorton & 3llu) << 2);
+    if (node.children[0] >= 0 && ((mode & morton) & 0x3lu) == 0) {
+        uint64_t childMode = mode & ~morton;
+        if (childMode == 0)
+            getEntitiesAll(node.children[0], entities);
+        else
+            getEntitiesBox(node.children[0], entities, begin, beginMorton >> 2, end, endMorton >> 2, childMode);
+    }
+    if (node.children[1] >= 0 && ((mode & (morton ^ 0x4lu)) & 0x6lu) == 0) {
+        uint64_t childMode = mode & (morton ^ 0xAlu);
+        if (childMode == 0)
+            getEntitiesAll(node.children[1], entities);
+        else
+            getEntitiesBox(node.children[1], entities, begin, beginMorton >> 2, end, endMorton >> 2, childMode);
+    }
+    if (node.children[2] >= 0 && ((mode & (morton ^ 0x8lu)) & 0x9lu) == 0) {
+        uint64_t childMode = mode & (morton ^ 0xClu);
+        if (childMode == 0)
+            getEntitiesAll(node.children[2], entities);
+        else
+            getEntitiesBox(node.children[2], entities, begin, beginMorton >> 2, end, endMorton >> 2, childMode);
+    }
+    if (node.children[3] >= 0 && ((mode & (morton ^ 0xClu)) & 0xClu) == 0) {
+        uint64_t childMode = mode & morton;
+        if (childMode == 0)
+            getEntitiesAll(node.children[3], entities);
+        else
+            getEntitiesBox(node.children[3], entities, begin, beginMorton >> 2, end, endMorton >> 2, childMode);
+    }
+}
+
+uint64_t WorldSingleton::createMortonCode(const Vec2f& p) const
 {
     float x = 1.0f + (p(0)+_worldSize)/(2.0f*_worldSize);
     float y = 1.0f + (p(1)+_worldSize)/(2.0f*_worldSize);
