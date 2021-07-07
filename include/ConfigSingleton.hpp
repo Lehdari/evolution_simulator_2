aace03cf604c83f2f757d7f66de2039c0e2a749d
Lehdari <miika.lehtimaki@gmail.com>
2021-07-08 01:00:32 +0300

    ConfigSingleton: Change creature movement constants

--- include/ConfigSingleton.hpp
+++ include/ConfigSingleton.hpp
@@ -21,9 +21,9 @@ struct ConfigSingleton {
     static constexpr double maxFoodMass = 16.0; // maximum mass a food can grow to
     static constexpr float  maxObjectRadius = 8.0f; // square root of max(maxCreatureMass, maxFoodMass)
 
-    double  creatureEnergyUseConstant = 0.05; // energy used every tick, relative to sqrt of mass
-    double  creatureMovementEnergyUseConstant = 0.2; // multiplier for energy used in moving (acceleration and turning)
-    float   creatureDragCoefficient = 0.5f; // viscous drag coefficient for creatures, viscous drag is relative to speed squared
+    double  creatureEnergyUseConstant = 0.1; // energy used every tick, relative to sqrt of mass
+    double  creatureMovementEnergyUseConstant = 0.1; // multiplier for energy used in moving (acceleration and turning)
+    float   creatureDragCoefficient = 0.9f; // viscous drag coefficient for creatures, viscous drag is relative to speed squared
     double  creatureMassIncreaseFactor = 0.1; // portion of food mass that is converted to creature mass when eaten (given the creature is still growing)
     double  massEnergyStorageConstant = 1000.0; // how much energy each creature can hold w.r.t. their mass
     double  foodMassToEnergyConstant = 100.0; // ratio by which food mass in converted to creature energy
